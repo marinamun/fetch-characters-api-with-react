d549d96d0cf113dd257c65a2b84dbb97f536d19f
Marina Muñoz <marinacerejidoo@gmail.com>
2023-10-22 12:00:15 +0200

    Did the POST

--- backend/routes/characters.routes.js
+++ backend/routes/characters.routes.js
@@ -1,32 +1,58 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
 /* GET home page */
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const responseFromAPI = await fetch('https://ih-crud-api.herokuapp.com/characters')
+    const responseFromAPI = await fetch(
+      "https://ih-crud-api.herokuapp.com/characters"
+    );
     if (responseFromAPI.ok) {
-      const charactersFromAPI = await responseFromAPI.json()
-      res.json({ characters: charactersFromAPI })
+      const charactersFromAPI = await responseFromAPI.json();
+      res.json({ characters: charactersFromAPI });
     }
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const responseFromAPI = await fetch(
       `https://ih-crud-api.herokuapp.com/characters/${req.params.id}`
-    )
+    );
     if (responseFromAPI.ok) {
-      const characterFromAPI = await responseFromAPI.json()
-      res.json({ character: characterFromAPI })
+      const characterFromAPI = await responseFromAPI.json();
+      res.json({ character: characterFromAPI });
     }
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 
-module.exports = router
+//Create a new character
+router.post("/characters", async (req, res) => {
+  try {
+    //Send the user's added data to the backend
+    const responseFromAPI = await fetch(
+      "https://ih-crud-api.herokuapp.com/characters",
+      {
+        method: "POST",
+        body: JSON.stringify(req.body),
+        headers: {
+          "Content-Type": "application-json",
+        },
+      }
+    );
+    //if the data was successfully entered in the DB, notify the user in the frontend
+    if (responseFromAPI.ok) {
+      const newCharacter = await responseFromAPI.json();
+      res.status(201).json(newCharacter); //201="Created", message to the user in the frontend
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
 
 // https://ih-crud-api.herokuapp.com/characters
